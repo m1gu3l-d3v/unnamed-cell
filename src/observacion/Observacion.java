0ea24b915e6213951b148f41c133ab20a964984c
m1gu3l-d3v <miguelangelp_10@outlook.com>
2025-01-27 14:54:56 -0500

    Add Getters and Setters for Observacion.java

--- src/observacion/Observacion.java
+++ src/observacion/Observacion.java
@@ -1,6 +1,31 @@
 package observacion;
+
 public class Observacion {
-  int idObservacion;
-  String autor;
-  String descripcion;
+  private int idObservacion;
+  private String autor;
+  private String descripcion;
+
+  public int getIdObservacion() {
+    return idObservacion;
+  }
+
+  public void setIdObservacion(int idObservacion) {
+    this.idObservacion = idObservacion;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+  public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
 }
